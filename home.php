3be7c9a0a437b335e0d55234c67885cfcf59cbf4
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-11 12:24:53 -0500

    Update home.php

--- home.php
+++ home.php
@@ -6,23 +6,31 @@
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
 <link rel="icon" type="image/x-icon" href="prettybraids.ico">
-<script type="text/javascript" src="script.js"></script>
 <link rel="stylesheet" type="text/css" href="styles.css">
+<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
-        
-            <nav>
-            <ul>
-            <li><a href="home.php">Home</a></li>
-            <li><a href="prices.php">Prices</a></li>
-            <li><a href="bookus.php">Book Us</a></li>
-            <li><img src="prettybraids.png" alt="Pretty Braids" width="200" height="200"></li>
-            <li><a href="myclients.php">My Clients</a></li>
-            <li><a href="about.php">About</a></li>
-            </ul>
-        </nav>
-
-            <?php echo "<h1>Pretty Braids</h1>"; ?>
-            <?php echo "<h2>Pretty Braids where we make Pretty Girls with Pretty Braids Feel Pretty!!</h2>"; ?>
+    
+<nav class="navbar">
+      <ul class="nav-links">
+        <div class="nav-links-left">
+          <li><a href="home.php">Home</a></li>
+          <li><a href="prices.php">Prices</a></li>
+          <li><a href="bookus.php">Book Us</a></li>
+        </div>
+        <div class="nav-links-right">
+          <li><a href="myclients.php">My Clients</a></li>
+          <li><a href="about.php">About</a></li>
+        </div>
+      </ul>
+      <div class="logo">
+        <img
+          src="prettybraids.png"
+          alt="Pretty Braids Logo"
+        />
+      </div>
+    </nav>
+      <h1>Pretty Braids</h1>
+      <h2>Pretty Braids where we make Pretty Girls with Pretty Braids Feel Pretty!!</h2>
 </body>
 </html>
