8f43022e7eb526e6a233e3804dc802c24444e4c3
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-14 12:03:54 -0500

    Update bookus.php

--- bookus.php
+++ bookus.php
@@ -15,35 +15,23 @@
             <?php echo "<h1><span><img src="prettybraids.png" alt="Pretty Braids Logo"></span>Book Now <span><img src="prettybraids.png" alt="Pretty Braids Logo"></span></h1>"; ?>
         </div>
             <form action="script.js" method="get">
-                <div class="form-group">
+                <div>
                     <span>Your full name:</span>
                     <input type="text" name="name" id="name" placeholder="" required>
                 </div>
-                <div class="form-group">
+                <div>
                     <span>Your email:</span>
                     <input type="email" name="name" id="name" placeholder="" required> 
                 </div>
-                <div class="form-group">
-                    
-                    <span>How many people ?</span>
-                    <select name="" id="" required>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                    </select>
-                    
-                </div>
-                <div class="form-group">
+                <div>
                     <span>What time:</span>
                     <input type="text" name="time" id="time" placeholder="Time" required>
                 </div>
-                <div class="form-group">
+                <div>
                     <span>Date:</span>
                     <input type="date" name="date" id="date" placeholder="date" required>
                 </div>
-                <div class="form-group">
+                <div>
                     <span>Phone Number:</span>
                     <input type="number" name="number" id="number" placeholder="Write your number here..." required>
                 </div>
