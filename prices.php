9607a179fc721a821070b050607e39b6c1495cfe
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-16 18:36:50 -0500

    Update prices.php

--- prices.php
+++ prices.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prices: Pretty Braids</title>
+<title>Prices: Pretty Braids</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="image/x-icon" href="prettybraids.ico">
+<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-<?php echo "<h1>Pretty Braids: Prices</h1>"; ?>    
+
+<nav class="navbar">
+      <ul class="nav-links">
+        <div class="nav-links-left">
+          <li><a href="home.php">Home</a></li>
+          <li><a href="prices.php">Prices</a></li>
+          <li><a href="bookus.php">Book Us</a></li>
+        </div>
+        <div class="nav-links-right">
+          <li><a href="myclients.php">My Clients</a></li>
+          <li><a href="about.php">About</a></li>
+        </div>
+      </ul>
+      <div class="logo">
+        <img
+          src="prettybraids.png"
+          alt="Pretty Braids Logo"
+        />
+      </div>
+    </nav>
+     <main>
+      <h1 style="color: #ff00b4">Prices</h1>
+      <h2 style="color: #ff00b4"></h2>
+      </main>
 </body>
 </html>
