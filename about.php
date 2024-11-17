e41fa87d279b581d16a52cee8eb5e770c55f3a71
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-16 21:39:03 -0500

    Update about.php

--- about.php
+++ about.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>About: Pretty Braids</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>About: Pretty Braids</title>
+<meta charset="UTF-8">
+<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
+<link rel="icon" type="image/x-icon" href="prettybraids.ico">
+<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-<?php echo "<h1>About</h1>"; ?>  
+  
+<nav class="navbar">
+      <ul class="nav-links">
+        <div class="nav-links-left">
+          <li><a href="home.php">Home</a></li>
+          <li><a href="prices.php">Prices</a></li>
+          <li><a href="bookus.php">Book Us</a></li>
+        </div>
+        <div class="nav-links-right">
+          <li><a href="myclients.php">My Clients</a></li>
+          <li><a href="about.php">About</a></li>
+        </div>
+      </ul>
+      <div class="logo">
+        <img
+          src="prettybraids.png"
+          alt="Pretty Braids Logo"
+        />
+      </div>
+    </nav>
+     <main>
+      <h1 style="color: #ff00b4">Pretty Braids</h1>
+      <p style="color: #ff00b4"></p>
+      </main>
 </body>
 </html>
