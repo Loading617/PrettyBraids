d32047c353027f366ca718ec3f355683cd39f67e
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-17 14:19:09 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Pretty Braids</title>
+<title>Pretty Braids: Pretty Braiders!</title>
 <meta charset="UTF-8">
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
