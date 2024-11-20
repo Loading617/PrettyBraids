ee2aa7c60fba369f27c291a6c476c2ef1b75896a
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-20 12:48:08 -0500

    Update myclients.php

--- myclients.php
+++ myclients.php
@@ -33,40 +33,40 @@
     </nav>
      <main>
       <div class="container">
-      <?php echo "<h1 style="color: #ff00b4">My Clients</h1>"; ?>
-      <?php echo "<p style="color: #ff00b4">Here are my Pretty Braiders!</p>"; ?>
+      <h1 style="color: #ff00b4">My Clients</h1>
+      <p style="color: #ff00b4">Here are my Pretty Braiders!</p>
       <div class="owl-carousel video-section">
     <div class="item">
       <div>
-    <video class="js-player" crossorigin playsinline poster=".jpg">
+    <video class="js-player" crossorigin playsinline poster="vid thumbnails/toolzin.com-Cwx2i6UuCbK.jpg">
 			<source src="vids/Snapinsta.app_video_D34D8538A3AEBF98A44214536EECC2B5_video_dashinit.mp4" type="video/mp4" size="720">
         </video>
 </div>
   </div>
       <div class="item">
       <div>
-      <video class="js-player" crossorigin playsinline poster=".jpg">
+      <video class="js-player" crossorigin playsinline poster="vid thumbnails/toolzin.com-CvS2N6BtHF8.jpg">
 			<source src="vids/Snapinsta.app_video_7F4FC3B9B96B90F404B93826B8E8E09A_video_dashinit.mp4" type="video/mp4" size="720">
         </video>
       </div>
   </div>
       <div class="item">
       <div>
-      <video class="js-player" crossorigin playsinline poster=".jpg">
+      <video class="js-player" crossorigin playsinline poster="vid thumbnails/toolzin.com-CuLHZjePKpP.jpg">
 			<source src="vids/Snapinsta.app_video_924C9E9A1829538A0731792AD36C4DA9_video_dashinit.mp4" type="video/mp4" size="720">
         </video>
       </div>
   </div>
   <div class="item">
       <div>
-    <video class="js-player" crossorigin playsinline poster=".jpg">
+    <video class="js-player" crossorigin playsinline poster="vid thumbnails/toolzin.com-CuKTNm5PBrC.jpg">
 			<source src="vids/Snapinsta.app_video_5B4A8826E68F0D14AE070412D6455FBD_video_dashinit.mp4" type="video/mp4" size="720">
         </video>
 </div>
   </div>
       <div class="item">
       <div>
-      <video class="js-player" crossorigin playsinline poster=".jpg">
+      <video class="js-player" crossorigin playsinline poster="vid thumbnails/toolzin.com-Cp3ws6at2UQ.jpg">
 			<source src="vids/Snapinsta.app_video_1443B1E471B1D4C95DF118B3494DB887_video_dashinit.mp4" type="video/mp4" size="720">
         </video>
       </div>
