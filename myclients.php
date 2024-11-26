3c1ba25c01cbb5b734802cadf2c631389a4a4c74
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-25 23:32:05 -0500

    Update myclients.php

--- myclients.php
+++ myclients.php
@@ -35,40 +35,21 @@
       <div class="container">
       <?php echo "<h1 style="color: #ff00b4">My Clients</h1>" ?>
       <?php echo "<p style="color: #ff00b4">Here are my Pretty Braiders!</p>"; ?>
+      <div class="container">
+      <h1 style="color: #ff00b4">My Clients</h1>
+      <p style="color: #ff00b4">Here are my Pretty Braiders!</p>
       <div class="video-container">
-  <a class="video-slider, videobox">
-    <div class="video-slide">
-      <video controls>
-        <source src="/vids/Snapinsta.app_video_D34D8538A3AEBF98A44214536EECC2B5_video_dashinit.mp4" type="video/mp4">
-      </video>
-    </div>
-    <div class="video-slide">
-      <video controls>
-        <source src="/vids/Snapinsta.app_video_1443B1E471B1D4C95DF118B3494DB887_video_dashinit.mp4" type="video/mp4">
-      </video>
-    </div>
-    <div class="video-slide">
-      <video controls>
-        <source src="/vids/Snapinsta.app_video_924C9E9A1829538A0731792AD36C4DA9_video_dashinit.mp4" type="video/mp4">
-      </video>
+      <div class="video-slider">
+    <div class="arrow arrow-left fa-solid fa-angle-left"></div>
+    <div class="arrow arrow-right fa-solid fa-angle-right"></div>
+    <div class="videos">
+      <video src="vids/Snapinsta.app_video_D34D8538A3AEBF98A44214536EECC2B5_video_dashinit.mp4" controls="controls">Smeduim straight backs w/ design 🥰</video>
+      <video src="vids/Snapinsta.app_video_7F4FC3B9B96B90F404B93826B8E8E09A_video_dashinit.mp4" controls="controls">Smedium knotless 🔥🔥 Appointments available!!</video>
+      <video src="vids/Snapinsta.app_video_924C9E9A1829538A0731792AD36C4DA9_video_dashinit.mp4" controls="controls">Quick style before the cookout!!!</video>
+      <video src="vids/Snapinsta.app_video_5B4A8826E68F0D14AE070412D6455FBD_video_dashinit.mp4" controls="controls">Knotless Box Braids</video>
+      <video src="vids/Snapinsta.app_video_1443B1E471B1D4C95DF118B3494DB887_video_dashinit.mp4" controls="controls"></video>
       </div>
-      <div class="video-slide">
-      <video controls>
-        <source src="/vids/Snapinsta.app_video_7F4FC3B9B96B90F404B93826B8E8E09A_video_dashinit.mp4" type="video/mp4">
-      </video>
-      </div>
-      <div class="video-slide">
-      <video controls>
-        <source src="/vids/Snapinsta.app_video_5B4A8826E68F0D14AE070412D6455FBD_video_dashinit.mp4" type="video/mp4">
-      </video>
-    </div>
-  </a>
-
-
-  <button class="video-slider-btn left-side" onclick="plusDivs(-1)">&#10094;</button>
-  <button class="video-slider-btn right-side" onclick="plusDivs(1)">&#10095;</button>
-</div>
-</div>
-      </main>
+  </div>
+  <script src="https://kit.fontawesome.com/a7731f7ef8.js"></script>
 </body>
 </html>
