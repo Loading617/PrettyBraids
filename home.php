dc91eb1dcb00fe4feb2bdb01cab42abdbc4c7f66
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-24 16:51:29 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Pretty Braids By: Jess</title>
+<title>Pretty Braids</title>
 <meta charset="UTF-8">
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
