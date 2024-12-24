bd96ff02152bbd392f1553828177aab9ac152aff
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-24 12:47:55 -0500

    Update home.php

--- home.php
+++ home.php
@@ -33,5 +33,31 @@
       <?php echo "<h1 style="color: #ff00b4">Pretty Braids</h1>"; ?>
       <?php echo "<p style="color: #ff00b4">Pretty Braids where we make Pretty Girls with Pretty Braids Feel Pretty!!</p>"; ?>
       </main>
+      <footer class="footer">
+  <div class="footer-container">
+    <div class="footer-column">
+      <?php echo"<h3>Pretty Braids</h3>"; ?>
+    <div class="footer-column">
+      <?php echo"<h3>Contact Us</h3>"; ?>
+      <ul>
+        <li><a href="tel:+8035616053">Phone</li>
+      </ul>
+    </div>
+    </div>
+    <div class="footer-column">
+      <?php echo"<h3>Follow us on</h3>"; ?>
+      <ul class="social-links">
+        <li><a href="https://www.facebook.com/profile.php?id=100092492316641">Facebook</a></li>
+        <li><a href="https://www.instagram.com/pretty_braids2020/">Instagram</a></li>
+        <li><a href="https://www.tiktok.com/@thegotojess?lang=en">TikTok</a></li>
+        <li><a href="https://youtube.com/@Pretty_Braids">YouTube</a></li>
+        <li><a href="https://youtube.com/@TheGoToJess">YouTube</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <?php echo"<p>&copy; Pretty Braids. All rights reserved.</p>"; ?>
+  </div>
+</footer>
 </body>
 </html>
