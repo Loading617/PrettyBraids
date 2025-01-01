49185a752d2850bd1515c51d2068c0151b063e33
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-01 18:17:24 -0500

    Update about.php

--- about.php
+++ about.php
@@ -39,31 +39,26 @@
         We strive to create a memorable and comfortable experience for our clients, making them feel confident and at ease with their new braids.
         Our mission is to provide high-quality, stylish, and customizable braids that cater to your specific needs and preferences.</p>"; ?>
       </main>
-      <footer class="footer">
-  <div class="footer-container">
-    <div class="footer-column">
-      <?php echo"<h3>Pretty Braids</h3>"; ?>
-    <div class="footer-column">
-      <?php echo"<h3>Contact Us</h3>"; ?>
-      <ul>
-        <li><a href="tel:+8035616053">Phone</li>
-      </ul>
-    </div>
-    </div>
-    <div class="footer-column">
-      <?php echo"<h3>Follow us on</h3>"; ?>
-      <ul class="social-links">
-        <li><a href="https://www.facebook.com/profile.php?id=100092492316641">Facebook</a></li>
+      <footer>
+  <div class="footer-section">
+    <h4>Contact Us</h4>
+    <ul>
+    <li><a href="tel:+8035616053">Phone</li>
+    </ul>
+  </div>
+
+  <div class="footer-section social-icons">
+    <h4>Follow us on</h4>
+    <ul>
+    <li><a href="https://www.facebook.com/profile.php?id=100092492316641">Facebook</a></li>
         <li><a href="https://www.instagram.com/pretty_braids2020/">Instagram</a></li>
         <li><a href="https://www.tiktok.com/@thegotojess?lang=en">TikTok</a></li>
         <li><a href="https://youtube.com/@Pretty_Braids">YouTube</a></li>
         <li><a href="https://youtube.com/@TheGoToJess">YouTube</a></li>
-      </ul>
-    </div>
+    </ul>
   </div>
-  <div class="footer-bottom">
-    <?php echo"<p>&copy; Pretty Braids. All rights reserved.</p>"; ?>
-  </div>
+
+  <p>&copy; Ferrero 2021. All rights reserved.</p>
 </footer>
 </body>
 </html>
