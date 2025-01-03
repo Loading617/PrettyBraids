ae02c79f90b63b3ba6bb12b08a871838f338f232
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-03 14:07:10 -0500

    Update about.php

--- about.php
+++ about.php
@@ -39,8 +39,29 @@
         We strive to create a memorable and comfortable experience for our clients, making them feel confident and at ease with their new braids.
         Our mission is to provide high-quality, stylish, and customizable braids that cater to your specific needs and preferences.</p>"; ?>
       </main>
-      <footer>
+      
+      <div class="footer">
 
-</footer>
+        <div class="footer-section">
+        <h4>Contact Us</h4>
+        <a href="tel:+8035616053">Phone</a>
+        </div>
+
+        <div class="footer-section">
+            <h4>Follow us on</h4>
+            <div class="social-icons">
+            <a href="https://www.facebook.com/profile.php?id=100092492316641">Facebook</a></li>
+            <a href="https://www.instagram.com/pretty_braids2020/">Instagram</a></li>
+            <a href="https://www.tiktok.com/@thegotojess?lang=en">TikTok</a></li>
+            <a href="https://youtube.com/@Pretty_Braids">YouTube</a></li>
+            <a href="https://youtube.com/@TheGoToJess">YouTube</a></li>
+            </div>
+        </div>
+
+        <div class="footer-section" style="flex-basis: 100%; text-align: center;">
+            <p>&copy; Pretty Braids 2020-2025. All rights reserved.</p>
+        </div>
+    </div>
+          
 </body>
 </html>
