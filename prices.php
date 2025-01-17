f4286b66a77b35ee5ec55a2b6944bf9764932379
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-17 05:06:14 -0500

    Update prices.php

--- prices.php
+++ prices.php
@@ -34,11 +34,11 @@
       <?php echo"<h1 style="color: #ff00b4">Prices</h1>"; ?>
       <?php echo"<p style="color: #ff00b4">Time to Take</p>"; ?>
         <ul style="color: #ff00b4;">
-            <li>$: </li>
-            <li>$: </li>
-            <li>$: </li>
-            <li>$: </li>
-            <li>$: </li>
+            <li>$ for: </li>
+            <li>$ for: </li>
+            <li>$ for: </li>
+            <li>$ for: </li>
+            <li>$ for: </li>
         </ul>
       </main>
 </html>
