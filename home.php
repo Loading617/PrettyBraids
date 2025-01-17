cdfa9a11288569fdcfea43a7226a8ed49448c25d
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-17 05:27:05 -0500

    Update home.php

--- home.php
+++ home.php
@@ -21,14 +21,13 @@
           <li><a href="myclients.php">My Clients</a></li>
           <li><a href="about.php">About</a></li>
         </div>
-      </ul>
+
       <div class="logo">
         <img
           src="prettybraids.png"
           alt="Pretty Braids Logo"
         />
-      </div>
-    </nav>
+          
       <main>
       <?php echo "<h1 style="color: #ff00b4">Pretty Braids</h1>"; ?>
       <?php echo "<p style="color: #ff00b4">Pretty Braids where we make Pretty Girls with Pretty Braids Feel Pretty!!</p>"; ?>
