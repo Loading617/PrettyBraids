04479fce5563ab982266eeeba6fb91193cdb8a3e
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-02 17:59:56 -0400

    Update myclients.php

--- myclients.php
+++ myclients.php
@@ -38,19 +38,19 @@
       <div class="spacer"></div>
 <div class="swimlane">
   <div class="tile">
-    <video src="https://cdn.discordapp.com/attachments/1357025469806936295/1357025471803429035/Snapinsta.app_video_5B4A8826E68F0D14AE070412D6455FBD_video_dashinit.mp4?ex=67eeb3f4&is=67ed6274&hm=a72734d85c20bdaab84437ca4bcb292dc885693b927d57b030f307819d0e9598&" autoplay loop muted></video>
+    <video src="vids/Snapinsta.app_video_D34D8538A3AEBF98A44214536EECC2B5_video_dashinit.mp4" autoplay loop muted></video>
   </div>
   <div class="tile">
-    <video src="https://cdn.discordapp.com/attachments/1357025469806936295/1357025472386433144/Snapinsta.app_video_7F4FC3B9B96B90F404B93826B8E8E09A_video_dashinit.mp4?ex=67eeb3f4&is=67ed6274&hm=c2375a4717364debf64c1ac27f87cd7e4c9bc4393faecfe36e2161c44661b8f3&" autoplay loop muted></video>
+    <video src="vids/Snapinsta.app_video_7F4FC3B9B96B90F404B93826B8E8E09A_video_dashinit.mp4" autoplay loop muted></video>
   </div>
   <div class="tile">
-    <video src="https://cdn.discordapp.com/attachments/1357025469806936295/1357025473204588754/Snapinsta.app_video_924C9E9A1829538A0731792AD36C4DA9_video_dashinit.mp4?ex=67eeb3f5&is=67ed6275&hm=89ce7f218d091f2d21a97e3491c08cd8f01b1d24b0556bbcf68a93c5b954b975&" autoplay loop muted></video>
+    <video src="vids/Snapinsta.app_video_924C9E9A1829538A0731792AD36C4DA9_video_dashinit.mp4" autoplay loop muted></video>
   </div>
   <div class="tile">
-    <video src="https://cdn.discordapp.com/attachments/1357025469806936295/1357025473829278026/Snapinsta.app_video_1443B1E471B1D4C95DF118B3494DB887_video_dashinit.mp4?ex=67eeb3f5&is=67ed6275&hm=bfd51b7c8e507266294fff15f6e85414eb08e0088bb1868e3acf32e5f7eb02d6&" autoplay loop muted></video>
+    <video src="vids/Snapinsta.app_video_5B4A8826E68F0D14AE070412D6455FBD_video_dashinit.mp4" autoplay loop muted></video>
   </div>
   <div class="tile">
-    <video src="https://cdn.discordapp.com/attachments/1357025469806936295/1357025474487914617/Snapinsta.app_video_D34D8538A3AEBF98A44214536EECC2B5_video_dashinit.mp4?ex=67eeb3f5&is=67ed6275&hm=11994f17ec4d354df8e97975e1cedc6feb22ff0b721476065204709d9c81b34e&" autoplay loop muted></video>
+    <video src="vids/Snapinsta.app_video_1443B1E471B1D4C95DF118B3494DB887_video_dashinit.mp4" autoplay loop muted></video>
   </div>
   </div>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
