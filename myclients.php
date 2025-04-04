35e9d2a9414f3b3686b13846f93331ae6d0990c8
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-03 20:43:31 -0400

    Update myclients.php

--- myclients.php
+++ myclients.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head <?php echo""; ?>>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>My Clients: Pretty Braids</title>
@@ -11,7 +11,7 @@
 <link rel="stylesheet" type="text/css" href="styles.css">
 <link rel="stylesheet" type="text/css" href="script.js">
 </head>
-<body>
+<body <?php echo""; ?>>
     
 <nav class="navbar">
       <ul class="nav-links">
