c44f428ed69778e49575fa2faa6f50624f739f85
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-08 13:02:16 -0400

    Update prices.php

--- prices.php
+++ prices.php
@@ -29,6 +29,7 @@
       </div>
     </nav>
      <main style="padding-top:300px;">
+     <?php echo"<h1 style="color: #ff00b4">Prices</h1>"; ?>
       <ul style="color: #ff00b4;">
             <li>Hairstyle $: </li>
             <li>Hairstyle $: </li>
