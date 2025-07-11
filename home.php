094de4a6fe163881de852ad6b4c474cdb8c435ed
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-07-11 10:50:23 -0400

    Update home.php

--- home.php
+++ home.php
@@ -6,15 +6,16 @@
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
 <link rel="icon" type="image/x-icon" href="prettybraids.ico">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
 <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
-<body>    
+<body>
 
 <nav class="navbar">
       <ul class="nav-links">
         <div class="nav-links-left">
           <li><a href="home.php" class="active">Home</a></li>
-          <li><a href="prices.php">Prices</a></li>
           <li><a href="bookus.php">Book Us</a></li>
         </div>
         <div class="nav-links-right">
@@ -30,12 +31,50 @@
       </div>
     </nav>
      <main style="display: flex">
-     <main style="padding-top: 100px;">
+     <main style="padding-top: 100px">
      <div class="image-container"></div>
       <img src="" alt="Pic of Jessica" width="" height="" />
       <?php echo"<h1 style="color: #ff00b4">Welcome to my Hair Booking Site!!</h1>"; ?>
       <?php echo"<h2 style="color: #ff00b4">Pretty Braids where we make Pretty Girls with Pretty Braids Feel Pretty!!</h2>"; ?>
       <?php echo"<h3 style="color: #ff00b4">Located in Columbia, SC</h3>"; ?>
-      </main>
+
+  <footer>
+    <div class="footer-container">
+      <div class="footer-column">
+        <div class="footer-column"></div><?php echo"<h1>Connect with Us</h1>"; ?>
+        <ul>
+          <li><a href="mailto:jessicalolitathebrand@gmail.com"><span class="lnr lnr-envelope"> jessicalolitathebrand@gmail.com</span></a></li>
+          <li><a href="tel:+8035616053"><span class="lnr lnr-phone"> (803) 561-6053</span></a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-container">
+  <div class="footer-column">
+    <div class="footer-column"><?php echo"<h2>Keep Up with Us</h2>"; ?>
+    <div class="footer-container">
+      <div class="footer-bottom">
+        <div class="footer-column">
+          <div>Pretty Braids Est. 2020</div>
+        </div>
+      </div>
+      
+      <div class="footer-container">
+      <div class="footer-column">
+        <div class="footer-column"></div>
+        </div>
+        <div class="socials">
+          <a href="https://www.facebook.com/profile.php?id=100092492316641" target="_blank"><i class="fab fa-facebook"></i></a>
+          <a href="https://www.instagram.com/pretty_braids2020/" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.tiktok.com/@thegotojess?lang=en" target="_blank"><i class="fab fa-tiktok"></i></a>
+          <a href="https://www.youtube.com/@Pretty_Braids" target="_blank"><i class="fab fa-youtube"></i></a>
+          <a href="https://www.youtube.com/@TheGoToJess" target="_blank"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-wrapper">
+      <div class="dots"></div>
+    </div>
+  </footer>
+</main>
 </body>
 </html>
