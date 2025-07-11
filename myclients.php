3d2425c8d02e5a72413e1a625e1fcd3dfe7fd21e
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-07-11 10:56:54 -0400

    Update myclients.php

--- myclients.php
+++ myclients.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head <?php echo""; ?>>
+<head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>My Clients: Pretty Braids</title>
@@ -9,15 +9,16 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
 <link rel="icon" type="image/x-icon" href="prettybraids.ico">
 <link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
 <link rel="stylesheet" type="text/css" href="script.js">
 </head>
-<body <?php echo""; ?>>
+<body>
     
 <nav class="navbar">
       <ul class="nav-links">
         <div class="nav-links-left">
           <li><a href="home.php">Home</a></li>
-          <li><a href="prices.php">Prices</a></li>
           <li><a href="bookus.php">Book Us</a></li>
         </div>
         <div class="nav-links-right">
@@ -55,9 +56,43 @@
 
 </head>
 
-<div class="logo-container">
-        <img src="prettybraids.png" alt="Pretty Braids">
-</div>
-    
+<footer>
+    <div class="footer-container">
+      <div class="footer-column">
+        <div class="footer-column"></div><?php echo"<h1>Connect with Us</h1>"; ?>
+        <ul>
+          <li><a href="mailto:jessicalolitathebrand@gmail.com"><span class="lnr lnr-envelope"> jessicalolitathebrand@gmail.com</span></a></li>
+          <li><a href="tel:+8035616053"><span class="lnr lnr-phone"> (803) 561-6053</span></a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-container">
+  <div class="footer-column">
+    <div class="footer-column"><?php echo"<h2>Keep Up with Us</h2>"; ?>
+    <div class="footer-container">
+      <div class="footer-bottom">
+        <div class="footer-column">
+          <div>Pretty Braids Est. 2020</div>
+        </div>
+      </div>
+      
+      <div class="footer-container">
+      <div class="footer-column">
+        <div class="footer-column"></div>
+        </div>
+        <div class="socials">
+          <a href="https://www.facebook.com/profile.php?id=100092492316641" target="_blank"><i class="fab fa-facebook"></i></a>
+          <a href="https://www.instagram.com/pretty_braids2020/" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.tiktok.com/@thegotojess?lang=en" target="_blank"><i class="fab fa-tiktok"></i></a>
+          <a href="https://www.youtube.com/@Pretty_Braids" target="_blank"><i class="fab fa-youtube"></i></a>
+          <a href="https://www.youtube.com/@TheGoToJess" target="_blank"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-wrapper">
+      <div class="dots"></div>
+    </div>
+  </footer>
+
 </body>
 </html>
